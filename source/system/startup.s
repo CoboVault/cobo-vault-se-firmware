03c1676fb92e07f08d8136462260379deb3db343
zhaozhe <evallo@qq.com>
2020-07-24 15:15:13 +0800

    modify memory stack and heap size

--- source/system/startup.s
+++ source/system/startup.s
@@ -6,8 +6,8 @@
 ; * @date:    
 ; *
 ; *****************************************************************************/
-Stack_Size      EQU     0x00000800
-Heap_Size       EQU     0x00002000
+Stack_Size      EQU     0x00001000
+Heap_Size       EQU     0x00001800
 ;__initial_sp    EQU    0x20003000	   ;config sp value
 
                 AREA    STACK, NOINIT, READWRITE, ALIGN=3
